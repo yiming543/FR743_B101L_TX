dcf4cac707940f2bc28e8b5c45a81c97092c9d27
yiming543 <yiming.shi@true-win.com>
2025-12-17 15:39:25 +0800

    V05
    // 20251217 V05 CS:B9EA
    // 更新DRL,POS編碼

---
+++ FR743_B101L_TX_V05.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FR743_B101L_TX_V05.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FR743_B101L_TX_V05.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=fr743b101ltxv05.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/fr743b101ltxv05.x.tar
